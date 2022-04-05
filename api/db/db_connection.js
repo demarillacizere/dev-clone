173e8d8f2d38dd8d056dd8b3f5312db3f01bed84
demizere <l.izere@alustudent.com>
2022-04-06 00:32:46 +0200

    adjusted the code structure

--- api/db/db_connection.js
+++ api/db/db_connection.js
@@ -1,11 +1,4 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Router = require("./routes")
-
-const app = express();
-
-app.use(express.json());
-
+var mongoose = require ('mongoose');
 mongoose.connect(
     `mongodb+srv://dema:8Gl8Oatk62zXPL7i@cluster0.1uax9.mongodb.net/toDoDatabase?retryWrites=true&w=majority`, 
   {
@@ -13,15 +6,11 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
-
+//test mongodb connection
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
 });
 
-app.use(Router);
-
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
-});
+module.exports = exports = mongoose;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const morgan = require('morgan')
-const tasksRoutes = require('./routes/taskRoutes')
+const tasksRoutes = require('./api/routes/taskRoutes')
+const mongoose = require("./api/db/db_connection");
 
 const app = express()
  
@@ -17,20 +17,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/v1', tasksRoutes)
-
-mongoose.connect(
-    `mongodb+srv://dema:8Gl8Oatk62zXPL7i@cluster0.1uax9.mongodb.net/toDoDatabase?retryWrites=true&w=majority`, 
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-//test mongodb connection
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
